3e2419b663f7c816477139e3e103c45f5cbe66b4
truds99 <dudscardoso9@gmail.com>
2023-12-17 21:47:54 -0300

    basic requirements done

--- script.js
+++ script.js
@@ -1,38 +1,6 @@
-let user, dataUser = {};
+let user, dataUser = {}, verifyLast, lastMsg;
+const content = document.querySelector(".content");
 
-function getMsgs() {
-    const promise = axios.get("https://mock-api.driven.com.br/api/v6/uol/messages");
-    promise.then(renderMsgs);
-}
-
-function renderMsgs(msgs) {
-    const content = document.querySelector(".content");
-    for (let i=0; i<30; i++) {
-        switch (msgs.data[i].type) {
-            case "status": 
-                content.innerHTML +=
-                `<div class="boxMsg status">
-                    <p><span class="time">(${msgs.data[i].time})</span> <span class="from">${msgs.data[i].from}</span> ${msgs.data[i].text}</p>
-                </div>`;
-                break;
-            case "message":
-                content.innerHTML +=
-                `<div class="boxMsg message">
-                    <p><span class="time">(${msgs.data[i].time})</span> <span class="from">${msgs.data[i].from}</span> para <span class="to">${msgs.data[i].to}</span> ${msgs.data[i].text}</p>
-                </div>`;
-                break;
-            case "private_message":
-                content.innerHTML +=
-                `<div class="boxMsg private_message">
-                    <p><span class="time">(${msgs.data[i].time})</span> <span class="from">${msgs.data[i].from}</span> reservadamente para <span class="to">${msgs.data[i].to}</span> ${msgs.data[i].text}</p>
-                </div>`;
-                break;
-            default:;   
-        }       
-    }
-    const lastMsg = content.lastChild;
-    lastMsg.scrollIntoView(); //not working
-}           
 
 function enterRoom() {
     do {
@@ -55,14 +23,63 @@ function enterRoomError(userError) {
     alert ("Não foi possível entrar na sala");
 }
 
+function getMsgs() {
+    const promise = axios.get("https://mock-api.driven.com.br/api/v6/uol/messages");
+    promise.then(renderMsgs);
+}
+
+function renderMsgs(msgs) {
+    content.innerHTML = "";
+    for (let i=0; i<msgs.data.length; i++) {
+        switch (msgs.data[i].type) {
+            case "status": 
+                content.innerHTML +=
+                `<div class="boxMsg status">
+                    <p><span class="time">(${msgs.data[i].time})</span> <span class="from">${msgs.data[i].from}</span> ${msgs.data[i].text}</p>
+                </div>`;
+                break;
+            case "message":
+                content.innerHTML +=
+                `<div class="boxMsg message">
+                    <p><span class="time">(${msgs.data[i].time})</span> <span class="from">${msgs.data[i].from}</span> para <span class="to">${msgs.data[i].to}</span> ${msgs.data[i].text}</p>
+                </div>`;
+                break;
+            case "private_message":
+                content.innerHTML +=
+                `<div class="boxMsg private_message">
+                    <p><span class="time">(${msgs.data[i].time})</span> <span class="from">${msgs.data[i].from}</span> reservadamente para <span class="to">${msgs.data[i].to}</span> ${msgs.data[i].text}</p>
+                </div>`;
+                break;
+            default:;   
+        }       
+    }
+    lastMsg = msgs.data[msgs.data.length - 1].time;
+    scrollDown(lastMsg);
+}           
+
+function sendMsg() {
+    let msgInput = document.querySelector("input").value;
+    document.querySelector("input").value = "";
+    let msg = {from: user, to: "Todos", text: msgInput, type: "message"};
+    const promise = axios.post("https://mock-api.driven.com.br/api/v6/uol/messages", msg);
+    promise
+        .catch(sendError)
+        .then(getMsgs);
+}
+
+function sendError() {
+    window.location.reload()
+}
+
 function keepOnline() {
     axios.post("https://mock-api.driven.com.br/api/v6/uol/status", dataUser);
 }
 
-function sendMsg() {
-    let msg = {from: user, to: "Todos", text: document.querySelector(input).value, type: message};
-    const promise = axios.post("https://mock-api.driven.com.br/api/v6/uol/messages", msg);
-    
+function scrollDown(lastMsg) {
+    if(lastMsg != verifyLast) {
+        content.lastChild.scrollIntoView();
+    }
+    verifyLast = lastMsg;
 }
 
 enterRoom();
@@ -70,4 +87,5 @@ enterRoom();
 setInterval(getMsgs, 3000);
 
 setInterval(keepOnline, 5000);
+
 
